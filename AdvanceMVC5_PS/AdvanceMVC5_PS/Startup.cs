c24e265e592f1682ae9fee95f3e313422d6a8f6a
albert18 <caps18@hotmail.com>
2018-05-16 07:27:15 -0400

    Own Application

---
+++ AdvanceMVC5_PS/AdvanceMVC5_PS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdvanceMVC5_PS.Startup))]
+namespace AdvanceMVC5_PS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
